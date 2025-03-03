5699ce7a83a73d461843d193db6e7bad600a5754
Keegan Sands <keegsands@gmail.com>
2025-03-02 20:30:55 -0500

    Add new method for game controller

--- src/main/java/org/keegsands/sportball/controller/GameController.java
+++ src/main/java/org/keegsands/sportball/controller/GameController.java
@@ -1,14 +1,28 @@
 package org.keegsands.sportball.controller;
 
+import org.keegsands.sportball.model.Game;
+import org.keegsands.sportball.model.Season;
+import org.keegsands.sportball.service.GameService;
+import org.keegsands.sportball.service.SeasonService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController()
 @RequestMapping("/games")
 public class GameController {
-    @GetMapping()
-    public String index() {
-        return "Greetings from teams controller!";
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+    @GetMapping
+    public ResponseEntity<List<Game>> getAllGames() {
+        List<Game> games = gameService.list();
+        return ResponseEntity.ok(games);
     }
 }
